26473cef727a13e9ea652cc9f4d8ed808aaa2450
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-08-02 23:05:34 -0300

    exercicio da piramide feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,11 +1,31 @@
-let n = 6
+// let n = 4
+// let linha = "";
+
+// for (let index = 0; index < n; index += 1) {
+//   linha += "*";
+// }
+
+// for (let index2 = n; index2 > 0; index2 -= 1) {
+//   console.log(linha);
+// }
+
+// let n = 4;
+// let linha = "";
+
+// for (let index = 0; index < n; index +=1) {
+//   linha += "*";
+//   console.log(linha);
+// }
+
+let n = 7;
 let linha = "";
+let espaço = "";
 
-for (let index = 0; index < n; index += 1) {
+for (let index = 0; index < n; index +=1) {
+  for (let contador = 0; contador < n - index; contador += 1) {
+    espaço += " ";
+  }
   linha += "*";
+  console.log(espaço + linha);
+  espaço = "";
 }
-
-for (let index2 = n; index2 > 0; index2 -= 1) {
-  console.log(linha);
-}
-
