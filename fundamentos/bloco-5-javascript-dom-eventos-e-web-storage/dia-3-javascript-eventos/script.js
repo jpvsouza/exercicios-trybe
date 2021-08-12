3212e2749fe2e5e76ddf86ecffe25aac36abb3ec
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-08-11 22:44:32 -0300

    adicao das tarefas no calendario

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,3 +1,13 @@
+createDaysOfTheWeek();
+createMonth();
+criaBotaoFeriado();
+criaBotaoSexta();
+zoom();
+tarefa("cozinhar");
+tarefa("lavar louça");
+tarefa("fazer janta");
+
+
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
@@ -11,7 +21,7 @@ function createDaysOfTheWeek() {
     };
   };
   
-  createDaysOfTheWeek();
+  
   
 function createMonth() {
   let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -35,16 +45,70 @@ function createMonth() {
   }
 }
 
-createMonth();
 
-function criaBotao(Feriados) {
+
+function criaBotaoFeriado(Feriados) {
   let botao = document.createElement("button");
   botao.id = "btn-holiday";
-  botao.innerHTML = "Botão do feriado"
+  botao.innerHTML = "Feriados"
 
   let div = document.querySelector(".buttons-container");
   div.appendChild(botao);
 }
 
-criaBotao();
+
+
+function criaBotaoSexta(Sexta) {
+  let botao = document.createElement("button");
+  botao.id = "btn-friday";
+  botao.innerHTML = "Sextas";
+
+  let div = document.querySelector(".buttons-container");
+  div.appendChild(botao);
+}
+
+
+
+function zoom() {
+  let amplia = document.querySelectorAll(".day");
+  for (index = 0; index < amplia.length; index += 1) {
+    amplia[index].addEventListener("mouseover", function(evento) {
+      evento.target.style.fontSize = "30px";
+    })
+    amplia[index].addEventListener("mouseleave", function(evento) {
+      evento.target.style.fontSize = "";
+    })
+  }
+}
+
+
+function tarefa(nomeDaTarefa) {
+  let novaTarefa = document.createElement("span");
+  novaTarefa.innerHTML = nomeDaTarefa + "<br>";
+  let div = document.querySelector(".my-tasks");
+  div.appendChild(novaTarefa);
+}
+
+
+
+function mostraFeriado() {
+  let botao = document.querySelector("#btn-holiday");
+  botao.addEventListener("click", function(){
+    let feriado = document.querySelectorAll(".holiday");
+    for (let index = 0; index < feriado.length; index +=1) {
+      feriado[index].style.backgroundColor = "blue";
+    }
+  })
+}
+
+function escondeFeriado() {
+  let botao = document.querySelector("#btn-holiday");
+  botao.addEventListener("click", function(){
+    let feriado = document.querySelectorAll(".holiday");
+    for (let index = 0; index < feriado.length; index +=1) {
+      feriado[index].style.backgroundColor = "";
+    }
+  })
+}
+
 
