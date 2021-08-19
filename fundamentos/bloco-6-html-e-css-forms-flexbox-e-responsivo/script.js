9378dd5b70cf3ecdf9a3e249645070cd43c7ee58
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-08-18 21:35:07 -0300

    fomulario pronto, falta o JS

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/script.js
@@ -0,0 +1,13 @@
+let estados = document.getElementById("estados");
+
+let arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+let arraySiglaEstado = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RO', 'RS', 'RR', 'SC', 'SE', 'SP', 'TO'];
+
+for (let index = 0; index < arrayTextoEstado.length; index += 1) {
+  let estado = document.createElement("option");
+  estado.innerHTML = arrayTextoEstado[index];
+  estado.value = arraySiglaEstado[index];
+
+  estados.appendChild(estado);
+}
