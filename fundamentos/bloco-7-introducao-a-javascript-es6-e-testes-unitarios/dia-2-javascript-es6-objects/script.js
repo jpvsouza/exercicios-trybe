142d5f3296b0269616bbd9f2fbf80d3c6ec1bb82
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-08-31 12:41:36 -0300

    enviando alterações para fazer novo exercicio

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -94,3 +94,27 @@ const order = {
 
   // Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
 
+//   const assert = require('assert'); // Sintaxe para incluir o módulo assert
+
+//   assert.strictEqual(50, 50); // Sem erros: 50 === 50
+//   //assert.strictEqual(50, 70); // AssertionError: 50 === 70
+
+// function division(x, y) {
+//   return x / y;
+// }
+
+// const expected = division(9, 3);
+
+// assert.strictEqual(expected, 3, 'Nove dividido por três é igual a três');
+
+const assert = require('assert');
+
+function add(a, b) {
+  return a + b;
+}
+
+const expected = add(1, 2);
+
+assert.ok(expected === 3, 'Um mais dois é igual a três'); // Checa se o primeiro argumento é verdadeiro
+assert.strictEqual(expected, 3, 'Um mais dois é igual a três'); // Checa se o primeiro e segundo argumentos são iguais em valor e tipo (===)
+assert.notStrictEqual(expected, 4, 'Um mais dois é igual a três (e não quatro!)'); // Checa se o primeiro e segundo argumentos são diferentes (!==)
