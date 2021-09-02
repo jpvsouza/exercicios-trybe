1c9a59b1eb3ffa1cbc46694f3a59d76fd990f36f
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-09-02 18:35:58 -0300

    mandando commit

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/testeSum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/testeSum.js
@@ -8,7 +8,12 @@ function sum(a, b) {
   return a + b;
 }
 
-assert.strictEqual(sum(4, 5), 9, 'Erro no primeiro teste');
-assert.strictEqual(sum(0, 0), 0, 'Erro no segundo teste');
-assert.strictEqual(sum(4, '5'), 9, 'Erro no terceiro teste');
-assert.throws(() => {sum(4, '5')}, /^Error: parameters must be numbers$/);
+const somaZeroEZero = sum(0, 0);
+const somaQuatroECinco = sum(4, 5);
+const somaNumberEString = sum(4, '5');
+
+
+assert.strictEqual(somaQuatroECinco, 9, 'Erro no primeiro teste');
+assert.strictEqual(somaZeroEZero, 0, 'Erro no segundo teste');
+assert.strictEqual(somaNumberEString, 9, 'Erro no terceiro teste');
+assert.throws(somaNumberEString, /^Error: parameters must be numbers$/);
