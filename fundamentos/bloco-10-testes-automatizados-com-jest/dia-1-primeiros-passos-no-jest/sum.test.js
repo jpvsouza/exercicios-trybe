91cf02aa55349841a009a42c7a6c4cb68e7281e0
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-09-21 10:48:40 -0300

    adicionado o jest

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/sum.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/sum.test.js
@@ -1,1 +1,10 @@
 const sum = require('./sum');
+
+describe('Testes da função sum', () => {
+    test('Testa se 4 + 5 = 9', () => {
+        expect(sum(4, 5)).toEqual(9);
+    });
+    // test();
+    // test();
+    // test();
+});
